53b56264b325ddc389bcf1d14e9a7ee99c4fb73c
Tuethu Tran <tterrytran95@gmail.com>
2022-07-28 07:36:00 -0700

    asdfsdfasdfasdfsasdf

--- saraiyascope/consumers.py
+++ saraiyascope/consumers.py
@@ -7,19 +7,21 @@ class MuseumConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
         prefix="http://localhost:8000/media/images/"
-        # for i in range(0, 3000):
-        #     img = 'frame'+str(i*10)+'.jpg'
-        #     res = self.send(json.dumps({'img':prefix+img}))
-        #     print('response:', res)
-        #     time.sleep(.10)
+        
+        for i in range(0, 3000):
+            img = 'frame'+str(i*10)+'.jpg'
+            res = self.send(json.dumps({'img':prefix+img}))
+            print('response:', res)
+            time.sleep(.10)
+            
         # await self.close()
         
-        while True:
-            with open('/Users/tuethutran/saraiya-scope-venv/saraiya-scope-v2/saraiyascope/state', 'r') as file:
-                file.seek(0)
-                img=file.readline().split(",")[1]
-                res = self.send(json.dumps({'img':prefix+img}))
-                print(img)
+        # while True:
+        #     with open('/Users/tuethutran/saraiya-scope-venv/saraiya-scope-v2/saraiyascope/state', 'r') as file:
+        #         file.seek(0)
+        #         img=file.readline().split(",")[1]
+        #         res = self.send(json.dumps({'img':prefix+img}))
+        #         print(img)
     
     def receive(self):
         pass
