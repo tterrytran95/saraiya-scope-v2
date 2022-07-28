f936c371580e50f08960c81f6465565ae675904e
Tuethu Tran <tterrytran95@gmail.com>
2022-07-28 13:43:28 -0700

    print dict

--- sensor.py
+++ sensor.py
@@ -51,9 +51,9 @@ i2c = board.I2C()
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
 prox1 = adafruit_vcnl4040.VCNL4040(tca[0])
-prox2 = adafruit_vcnl4040.VCNL4040(tca[7])
+# prox2 = adafruit_vcnl4040.VCNL4040(tca[7])
 prox3 = adafruit_vcnl4040.VCNL4040(tca[4])
-prox4 = adafruit_vcnl4040.VCNL4040(tca[3])
+# prox4 = adafruit_vcnl4040.VCNL4040(tca[3])
 
 current = 'prox1'
 previous = None
@@ -72,20 +72,20 @@ while True:
         current = 'prox1'
         max_value = p1
     
-    p2 = prox2.proximity
-    if p2 > max_value:
-        current = 'prox2'
-        max_value = p2
+    # p2 = prox2.proximity
+    # if p2 > max_value:
+    #     current = 'prox2'
+    #     max_value = p2
     
     p3 = prox3.proximity
     if p3 > max_value:
         current = 'prox3'
         max_value = p3
         
-    p4 = prox4.proximity
-    if p4 > max_value:
-        current = 'prox4'
-        max_value = p4
+    # p4 = prox4.proximity
+    # if p4 > max_value:
+    #     current = 'prox4'
+    #     max_value = p4
 
     new_state = get_state(current, previous)
     if new_state in state_count_dict:
