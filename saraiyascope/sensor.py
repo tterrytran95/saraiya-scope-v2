957dbc3bbc5c739bf2f36e7c34d2a5a3e746fcfa
Tuethu Tran <tterrytran95@gmail.com>
2022-07-29 13:58:10 -0700

    refactored to sensor_utils

---
+++ saraiyascope/sensor.py
@@ -0,0 +1,82 @@
+# usage python3 sensor.py HOST=10.0.0.21
+
+import time, board, sys, socket, json
+from sensor_utils import *
+
+# HOST = '10.0.0.211' # ip address of displaying computer # terry's house
+# HOST = '192.168.42.54' # ip address of displaying computer # alex's house 
+# HOST = '192.168.1.26' # ip address of displaying computer # baltic mill
+
+
+HOST = sys.argv[1].split("=")[1]
+PORT = 1234
+TOTAL_FRAMES = 3080 # temp for now
+SAMPLE_RATE = 1000
+THRESH = 30
+
+## Step 1: Make socket connection to server ## 
+ClientSocket = socket.socket()
+print('Waiting for connection')
+try:
+    ClientSocket.connect((HOST, PORT))
+except socket.error as e:
+    print(str(e))
+    exit(1)
+Response = ClientSocket.recv(1024)
+
+## Step 2: Initialize sensors ## 
+initialize_sensors()
+
+## Step 3: Start sensors ## 
+current = 'prox1'
+previous = None
+img_count = 0 # send this to host so they know which image to render
+current_state = None
+current_sample = 0
+state_count_dict = {}
+while True:
+    
+    previous = current 
+    max_value = -1
+    
+    test_d = {
+        # 'p1' : prox1.proximity_high_interrupt,
+        # 'p2' : prox2.proximity_high_interrupt,
+        # 'p3' : prox3.proximity_high_interrupt,
+        # 'p4' : prox4.proximity_high_interrupt,
+        'p1' : prox1.proximity,
+        'p2' : prox2.proximity,
+        'p3' : prox3.proximity,
+        'p4' : prox4.proximity,
+    }
+    print(test_d)
+    current = get_trending_state(test_d)
+    new_state = get_state(current, previous)
+    state_count_dict = update_trend(state_count_dict, new_state)
+
+    if current_sample % SAMPLE_RATE == 0:
+        current_state = get_trending_state(state_count_dict)
+        print(current_state)
+        print(state_count_dict)
+        state_count_dict = {} # reset this
+        
+        if current_state == 'forward': 
+            img_count = (img_count + 1) % TOTAL_FRAMES
+            Input = current_state + ',' + 'frame'+str(img_count*10)+'.jpg'
+            ClientSocket.send(str.encode(Input))
+            Response = ClientSocket.recv(1024)
+        elif current_state == 'backward': 
+            img_count = (img_count - 1) % TOTAL_FRAMES
+            Input = current_state + ',' + 'frame'+str(img_count*10)+'.jpg'
+            ClientSocket.send(str.encode(Input))
+            Response = ClientSocket.recv(1024)
+        else:
+            Input = current_state + ',' + 'frame'+str(img_count*10)+'.jpg'
+            ClientSocket.send(str.encode(Input))
+            Response = ClientSocket.recv(1024)
+
+    current_sample += 1
+    time.sleep(.15)
+
+
+ClientSocket.close()
---
+++ saraiyascope/utils/sensor_utils.py
@@ -0,0 +1,59 @@
+import qwiic_tca9548a
+import qwiic_proximity
+import adafruit_tca9548a
+import adafruit_vcnl4040
+
+# utils for sensor
+def initialize_sensors():
+    # Create I2C bus as normal
+    i2c = board.I2C()
+
+    # Create the TCA9548A object and give it the I2C bus
+    tca = adafruit_tca9548a.TCA9548A(i2c)
+
+    prox1 = adafruit_vcnl4040.VCNL4040(tca[7])
+    prox2 = adafruit_vcnl4040.VCNL4040(tca[0])
+    prox3 = adafruit_vcnl4040.VCNL4040(tca[3])
+    prox4 = adafruit_vcnl4040.VCNL4040(tca[4])
+
+    prox1.proximity_high_threshold=20
+    prox2.proximity_high_threshold=20
+    prox3.proximity_high_threshold=20
+    prox4.proximity_high_threshold=20
+
+    prox1.proximity_interrupt=prox1.PS_INT_CLOSE
+    prox2.proximity_interrupt=prox2.PS_INT_CLOSE
+    prox3.proximity_interrupt=prox3.PS_INT_CLOSE
+    prox4.proximity_interrupt=prox4.PS_INT_CLOSE
+
+
+## determines the state of the sensors
+def get_state(current, previous):
+    if current == previous:
+        return 'stable'
+    
+    if current > previous:
+        return 'forward'
+    
+    if current < previous:
+        return 'backward'
+    
+def get_trending_state(count_dict):
+    max_value = -1
+    state = 'stable'
+    for k in count_dict:
+        if count_dict[k] > max_value:
+            state = k
+            max_value = count_dict[k]
+    return state
+
+def update_trend(state_count_dict, new_state):
+    if new_state in state_count_dict:
+        state_count_dict[new_state] += 1
+    else:
+        state_count_dict[new_state] = 1
+        
+    for k in state_count_dict:
+        if k != new_state:
+            state_count_dict[k] = state_count_dict[k] - 1 if (state_count_dict[k] - 1 > 0) else 0
+    return state_count_dict  
