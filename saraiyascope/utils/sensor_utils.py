a487ff72e3e14aa6afb41dc67f958a97b4d070a1
Tuethu Tran <tterrytran95@gmail.com>
2022-07-29 16:05:47 -0700

    set integration time

--- saraiyascope/utils/sensor_utils.py
+++ saraiyascope/utils/sensor_utils.py
@@ -24,10 +24,10 @@ def initialize_sensors():
     prox3.proximity_high_threshold=20
     prox4.proximity_high_threshold=20
     
-    prox7.proximity_integration_time=prox7.PS_4T
-    prox0.proximity_integration_time=prox0.PS_4T
-    prox3.proximity_integration_time=prox3.PS_4T
-    prox4.proximity_integration_time=prox4.PS_4T
+    prox7.proximity_integration_time=prox7.PS_1_5T
+    prox0.proximity_integration_time=prox0.PS_1_5T
+    prox3.proximity_integration_time=prox3.PS_1_5T
+    prox4.proximity_integration_time=prox4.PS_1_5T
 
     prox7.proximity_interrupt=prox7.PS_INT_CLOSE
     prox0.proximity_interrupt=prox0.PS_INT_CLOSE
