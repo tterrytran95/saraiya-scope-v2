557ead6359de3e029a981eba9562fc60071f7a84
Tuethu Tran <tterrytran95@gmail.com>
2022-08-01 07:00:48 -0700

    asdf

--- saraiyascope/consumers.py
+++ saraiyascope/consumers.py
@@ -1,7 +1,8 @@
 import json
 import time
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-STATE = "C:/Users/admin/projects/saraiya-scope-v2/saraiyascope/state" # path on baltic mill
+STATE = "/Users/tuethutran/saraiya-scope-venv/saraiya-scope-v2/saraiyascope/" # path on baltic mill
+# STATE = "C:/Users/admin/projects/saraiya-scope-v2/saraiyascope/state" # path on baltic mill
 
 # museum consumer for django channel
 class MuseumConsumer(WebsocketConsumer):
