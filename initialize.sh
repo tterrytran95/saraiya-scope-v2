9f1e279551a13181dfd19f75cc5139bcc70f2bc9
Terry Tran <tterrytran95@gmail.com>
2023-06-29 17:38:41 -0700

    removed virtual environment and packed into initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 # initialize the bioscope # only run this once
+echo "setting up virtual environment"
+python3 -m venv bioscope
+source bioscope/bin/activate
+pip install -r requirements.txt
 
 # use port 8889 to upload images to frontend
 echo "starting front end..."
--- server.sh
+++ server.sh
@@ -3,6 +3,8 @@
 # automate ssh into raspberry pi
 # https://medium.com/@eanunez85/automate-your-ssh-login-to-remote-servers-28dbdc0ed28f
 
+source bioscope/bin/activate
+
 # check if anything is running on port 1234 and kill it
 kill $(lsof -t -i:1234)
 
